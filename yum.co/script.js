5f7d85e64463c2f6ff8c3ea198fff64a2f79502c
raynold-ray <accdnd9@gmail.com>
2022-12-07 15:32:13 +0300

    More Improvements
    improved cart system

--- yum.co/script.js
+++ yum.co/script.js
@@ -3,14 +3,143 @@ const btnClose = document.getElementById("btnClose");
 const btnSearch = document.getElementById("btnSearch");
 const links = document.getElementById("links");
 const btnMenu = document.getElementById("menu");
-//const btnFav = document.getElementById("fav");
+const cCart = document.getElementById("cCart");
+const account = document.getElementById("account");
+const sSign = document.getElementById("sSign");
+const btncClose = document.getElementById("cClose");
+const btnsClose = document.getElementById("sClose");
 const addCart = document.getElementById("addCart");
-
-searchDiv.classList.toggle("disable");
+const viewCart = document.getElementById("viewCart");
+const vViewCart = document.getElementById("vViewCart");
+const total = document.getElementById("total");
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const dmap = document.getElementById("dmap");
+const gMap = document.getElementById("gMap");
+const parent = document.querySelector(".parent");
 links.classList.toggle("disable");
+
+
+
+let itemsInCart = JSON.parse(localStorage.getItem("Cart"));
+let sum = 0;
+let products = document.querySelectorAll(".item");
+if(!itemsInCart){itemsInCart = []};
+const getAllSum = function(){
+    let alsum = 0;
+    itemsInCart.forEach(product =>{
+        alsum += product.price;
+    });
+    return alsum;
+}
+
+const updateCartHtml = function(){
+    localStorage.setItem("Cart", JSON.stringify(itemsInCart));
+    if(itemsInCart.length > 0){
+        let result = itemsInCart.map(product =>{
+            return `
+            <li>
+                <img src="${product.image}">
+                <div>
+                    <h5>${product.name}</h5>
+                    <h6>Ksh. ${product.price}</h6>
+                    <div>
+                        <i class="fa fa-minus-circle" data-id="${product.id}" id="minus" aria-hidden="true"></i>
+                        <span id="quantity">${product.count}</span>
+                        <i class="fa fa-plus-circle" data-id="${product.id}" id="plus" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </li>
+            `
+        });
+        parent.innerHTML = result.join("");
+
+        document.querySelector(".cButton").classList.remove("disable");
+        total.innerHTML = `<h3 class="total" id="total">Total: <span>Ksh. ${getAllSum()}</span></h3>`
+    }else{
+        document.querySelector(".cButton").classList.add("disable");
+        //no items in cart
+        total.innerText = "No items!"
+    }
+}
+
+
+function updateCartProducts(product){
+    for(let i = 0; i < itemsInCart.length; i++){
+        if(itemsInCart[i].id == product.id){
+            itemsInCart[i].count += 1;
+            itemsInCart[i].price = itemsInCart[i].basePrice * itemsInCart[i].count;
+            return;
+        }
+    }
+    itemsInCart.push(product);
+    //alert("(" + itemsInCart.length + ")");
+}
+
+
+
+
+
+products.forEach(product => {
+    product.addEventListener("click", (e) =>{
+        if(e.target.classList.contains('orderitem')){
+            const productID = e.target.dataset.productId;
+            const productName = product.querySelector(".productName").innerText;
+            const productPrice = product.querySelector(".price").innerText.substring(4);
+            const productImage = product.querySelector("img").src;
+            let pCart = {
+                name: productName,
+                image: productImage,
+                id: productID,
+                count: 1,
+                price: +productPrice,
+                basePrice: +productPrice
+            }
+            updateCartProducts(pCart);
+            
+            updateCartHtml();
+        }
+    })
+})
+
+
+
+
 
 btnClose.addEventListener("click", () => {
     searchDiv.classList.toggle("disable");
+});
+
+dmap.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    gMap.style.transformOrigin = `${x}px ${y}px`;
+    gMap.style.transform = 'scale(2)';
+});
+
+dmap.addEventListener("mouseleave", () => {
+    gMap.style.transform = 'scale(1)';;
+});
+
+btncClose.addEventListener("click", () => {
+    cCart.classList.toggle("disable");
+});
+
+btnsClose.addEventListener("click", () => {
+    sSign.classList.toggle("disable");
+});
+
+account.addEventListener("click", () => {
+    sSign.classList.toggle("disable");
+});
+
+viewCart.addEventListener("click", () => {
+    cCart.classList.toggle("disable");
+});
+
+vViewCart.addEventListener("click", () => {
+    cCart.classList.toggle("disable");
 });
 
 btnSearch.addEventListener("click", () => {
@@ -20,3 +149,26 @@ btnSearch.addEventListener("click", () => {
 btnMenu.addEventListener("click", () => {
     links.classList.toggle("disable");
 });
+
+parent.addEventListener("click", (e) =>{
+    const reducing = e.target.classList.contains("fa-minus-circle");
+    const adding = e.target.classList.contains("fa-plus-circle");
+    if(adding || reducing){
+        for(i = 0; i < itemsInCart.length; i++){
+            if(itemsInCart[i].id == e.target.dataset.id){
+                if(adding){
+                    itemsInCart[i].count += 1;
+                }else{
+                    itemsInCart[i].count -= 1;
+                }
+                itemsInCart[i].price = itemsInCart[i].basePrice * itemsInCart[i].count;
+            }
+            if(itemsInCart[i].count <= 0){
+                itemsInCart.splice(i, 1);
+            }
+        }
+        updateCartHtml();
+    }
+})
+
+updateCartHtml();
